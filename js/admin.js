3080d118cf3af3ff532d39c4553f82609716c60f
Hobin Ryu <hobincar@kaist.ac.kr>
2017-05-19 16:11:59 +0900

    asdf

--- js/admin.js
+++ js/admin.js
@@ -37,7 +37,44 @@ $(document)
 
     }
 
-  })
+  });
+  
+  
+  $('.ui.manage-customer-reservation.form')
+  .form({
+    fields: {
+      email: {
+        identifier: 'email',
+        rules: [
+          {
+            type   : 'regExp',
+            value : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          }
+        ]
+      },
+      password: {
+        identifier: 'password',
+        rules: [
+          {
+            type   : 'empty',
+            prompt : 'Please enter your password'
+          }
+        ]
+      },
+    },
+    onSuccess: () => {
+      if ($('#request-type').text().trim() == "Request Type")
+        alert('Please select request type');
+      else if ($('#request-type').text().trim() == "Get a Reservation Information")
+        window.location.href = 'mypage.html';
+      else if ($('#request-type').text().trim() == "Make a Reservation")
+        window.location.href = 'reservation_step1.php';
+      else if ($('#request-type').text().trim() == "Edit a Reservation")
+        window.location.href = 'mypage.html';
+      else if ($('#request-type').text().trim() == "Cancel a Reservation")
+        window.location.href = 'mypage.html';
+    }
+  });
 
 })
 ;
